529c126ff5dd075f20a64788a813f6172583e0d9
mrcocoball <gunmaru93@gmail.com>
2023-01-10 21:37:23 +0900

    #1 - 프로젝트 빌드

---
+++ src/main/java/com/cocoball/kakaoapipractice/KakaoapipracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cocoball.kakaoapipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoapipracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaoapipracticeApplication.class, args);
+    }
+
+}
