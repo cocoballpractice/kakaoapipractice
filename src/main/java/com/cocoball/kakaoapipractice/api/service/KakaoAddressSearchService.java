59afa06a05af9aed245c4a4f3ab4cc1e006460ae
mrcocoball <gunmaru93@gmail.com>
2023-01-12 23:04:23 +0900

    #3 - Spring Retry 적용

--- src/main/java/com/cocoball/kakaoapipractice/api/service/KakaoAddressSearchService.java
+++ src/main/java/com/cocoball/kakaoapipractice/api/service/KakaoAddressSearchService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +27,11 @@ public class KakaoAddressSearchService {
     @Value("${kakao.rest.api.key}")
     private String kakaoRestApiKey;
 
+    @Retryable(
+            value = {RuntimeException.class}, // RuntimeException 발생 시
+            maxAttempts = 2, // 초기 요청 포함 2회 재요청
+            backoff = @Backoff(delay = 2000) // 딜레이 2초
+    )
     public KakaoApiResponseDto requestAddressSearch(String address) {
 
         // validation
@@ -40,6 +48,12 @@ public class KakaoAddressSearchService {
         // Kakao API 호출
         // exchange(URI url, HttpMethod method, @Nullable HttpEntity<?> requestEntity, Class<T> responseType) throws RestClientException
         return restTemplate.exchange(uri, HttpMethod.GET, httpEntity, KakaoApiResponseDto.class).getBody();
+    }
+
+    @Recover // Retry 실패 시 처리 메서드, 주의점 : 원래 메서드의 반환 타입을 반환해야 함
+    public KakaoApiResponseDto recover(RuntimeException e, String address) {
+        log.error("모든 에러가 실패하였음. address: {}, error: {}", address, e.getMessage());
+        return null;
     }
 
 }
