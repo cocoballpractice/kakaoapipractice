c502a9b57b0212fae39b1e9e6810902dd91dd3df
mrcocoball <gunmaru93@gmail.com>
2023-01-12 22:38:34 +0900

    #3 - DTO 추가 및 수정

--- src/main/java/com/cocoball/kakaoapipractice/api/dto/DocumentDto.java
+++ src/main/java/com/cocoball/kakaoapipractice/api/dto/DocumentDto.java
@@ -2,12 +2,14 @@ package com.cocoball.kakaoapipractice.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class DocumentDto {
 
     /**
