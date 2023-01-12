92637a70ea245e836c6148b233fc02f1ebe54f3e
mrcocoball <gunmaru93@gmail.com>
2023-01-12 23:04:15 +0900

    #3 - Spring Retry 관련 세팅

---
+++ src/main/java/com/cocoball/kakaoapipractice/config/RetryConfig.java
@@ -0,0 +1,19 @@
+package com.cocoball.kakaoapipractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.support.RetryTemplate;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+
+    /* RetryTemplate 사용 시
+
+    @Bean
+    public RetryTemplate retryTemplate() {
+        return new RetryTemplate();
+    }
+     */
+}
